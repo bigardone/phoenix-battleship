e8820a52e1b2f5c1528032b5cde35d31c19f132a
Ricardo García Vega <bigardone@gmail.com>
2016-03-31 09:04:11 +0200

    Game channel join

---
+++ test/channels/game_channel_test.exs
@@ -0,0 +1,35 @@
+defmodule Battleship.GameChannelTest do
+  @moduledoc false
+  use Battleship.ChannelCase, async: true
+  alias Battleship.Game.Supervisor, as: GameSupervisor
+  alias Battleship.{PlayerSocket, GameChannel}
+
+  @player_id "player-1"
+  @player_name "John"
+
+  setup do
+    game_id = GameSupervisor.generate_id
+
+    {:ok, game} = GameSupervisor.create_game(game_id)
+    {:ok, socket} = connect(PlayerSocket, %{"id" => @player_id, "name" => @player_name})
+
+    {:ok, game_id: game_id, game: game, socket: socket}
+  end
+
+  test "joining an invalid game channel", %{socket: socket} do
+    assert {:error, %{reason: "Game does not exist"}} = subscribe_and_join(socket, GameChannel, "game:invalid")
+  end
+
+  test "leaving the game channel kills the game", %{game_id: game_id, game: game, socket: socket} do
+    game_ref = Process.monitor(game)
+
+    {:ok, _, socket} = subscribe_and_join(socket, GameChannel, "game:" <> game_id)
+
+    Process.unlink(socket.channel_pid)
+
+    ref = leave(socket)
+    assert_reply ref, :ok
+
+    assert_receive {:DOWN, ^game_ref, :process, ^game, _}
+  end
+end
--- web/channels/game_channel.ex
+++ web/channels/game_channel.ex
@@ -1,7 +1,20 @@
 defmodule Battleship.GameChannel do
+  @moduledoc """
+  Game channel
+  """
   use Phoenix.Channel
+  alias Battleship.{Game}
+  require Logger
 
   def join("game:" <> game_id, _message, socket) do
+    Logger.debug "Joining Game channel", game_id: game_id
 
+    case Game.join(game_id, socket.assigns.player, socket.channel_pid) do
+      {:ok, pid} ->
+        # Process.link(pid)
+        {:ok, assign(socket, :game_id, game_id)}
+      {:error, reason} ->
+        {:error, %{reason: reason}}
+    end
   end
 end
