6ea2cb40989b8a4ceed5bed60d95d757a74ea004
Ricardo García Vega <bigardone@gmail.com>
2016-03-31 18:14:59 +0200

    User boards

---
+++ lib/battleship/game/board.ex
@@ -0,0 +1,48 @@
+defmodule Battleship.Game.Board do
+  @moduledoc """
+  Game board
+  """
+  alias Battleship.{Ship}
+  require Logger
+
+  @ships_sizes [5, 4, 3, 3, 2]
+  @size 10
+  @orientations [:horizontal, :vertical]
+
+  defstruct [
+    player_id: nil,
+    ships: [],
+    shots: []
+  ]
+
+  @doc """
+  Creates a new board for a Player
+  """
+  def create(player_id) do
+    Logger.debug "Starting board for player #{player_id}"
+
+    Agent.start(fn -> %__MODULE__{player_id: player_id} end, name: ref(player_id))
+  end
+
+  @doc """
+  Destroys an existing Player board
+  """
+  def destroy(player_id) do
+    Logger.debug "Stopping board for player #{player_id}"
+
+    Agent.stop(ref(player_id), :normal, :infinity)
+  end
+
+  @doc """
+  Adds a new ship to the board
+  """
+  def set_ship(_player_id, %Ship{size: size}) when size > 5 or size < 2, do: {:error, "Invalid size"}
+  def set_ship(_player_id, %Ship{x: x}) when x > (@size - 1) or x < 0, do: {:error, "Invalid position"}
+  def set_ship(_player_id, %Ship{y: y}) when y > (@size - 1) or y < 0, do: {:error, "Invalid position"}
+  def set_ship(_player_id, %Ship{orientation: orientation}) when not orientation in @orientations, do: {:error, "Invalid orientation"}
+  def set_ship(player_id, ship) do
+    
+  end
+
+  defp ref(player_id), do: {:global, {:board, player_id}}
+end
