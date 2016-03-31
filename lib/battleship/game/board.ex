5b0843e82ad9df1a6239560b018fae14289efcf6
Ricardo García Vega <bigardone@gmail.com>
2016-03-31 20:10:25 +0200

    Adding ships to board tests

--- lib/battleship/game/board.ex
+++ lib/battleship/game/board.ex
@@ -36,20 +36,34 @@ defmodule Battleship.Game.Board do
   @doc """
   Adds a new ship to the board
   """
-  def set_ship(_player_id, %Ship{size: size}) when size > 5 or size < 2, do: {:error, "Invalid size"}
-  def set_ship(_player_id, %Ship{x: x}) when x > (@size - 1) or x < 0, do: {:error, "Invalid position"}
-  def set_ship(_player_id, %Ship{y: y}) when y > (@size - 1) or y < 0, do: {:error, "Invalid position"}
-  def set_ship(_player_id, %Ship{orientation: orientation}) when not orientation in @orientations, do: {:error, "Invalid orientation"}
-  def set_ship(player_id, ship) do
+  def add_ship(_player_id, %Ship{size: size}) when not size in @ships_sizes, do: {:error, "Invalid size"}
+  def add_ship(_player_id, %Ship{x: x}) when x > (@size - 1) or x < 0, do: {:error, "Invalid position"}
+  def add_ship(_player_id, %Ship{y: y}) when y > (@size - 1) or y < 0, do: {:error, "Invalid position"}
+  def add_ship(_player_id, %Ship{orientation: orientation}) when not orientation in @orientations, do: {:error, "Invalid orientation"}
+  def add_ship(player_id, ship) do
+    Logger.debug "Adding ship for player #{player_id}"
+
     board = Agent.get(ref(player_id), &(&1))
 
     cond do
       length(board.ships) == length(@ships_sizes) ->
         {:error, "All ships are placed"}
+      ship_already_placed?(board, ship) ->
+        {:error, "Ship already added"}
       true ->
         Agent.update(ref(player_id), &(%{&1 | ships: [ship | &1.ships]}))
+
+        {:ok, ship}
     end
   end
 
   defp ref(player_id), do: {:global, {:board, player_id}}
+
+  @doc """
+  Checks if a similar ship has been already placed
+  """
+  defp ship_already_placed?(%__MODULE__{ships: ships}, %Ship{size: size}) do
+    permited_amount = Enum.count(@ships_sizes, &(&1 == size))
+    Enum.count(ships, &(&1.size == size)) == permited_amount
+  end
 end
---
+++ test/lib/game/board_test.exs
@@ -0,0 +1,44 @@
+defmodule Battleship.Game.BoardTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  alias Battleship.Game.Board
+  alias Battleship.Ship
+
+  @player_id "player-1"
+
+  setup do
+    {:ok, board} = Board.create(@player_id)
+
+    on_exit fn ->
+      Board.destroy(@player_id)
+    end
+
+    {:ok, board: board}
+  end
+
+  test "adding invalid ships" do
+    invalid_size_ship = %Ship{size: 10}
+    assert {:error, "Invalid size"} = Board.add_ship(@player_id, invalid_size_ship)
+
+    invalid_position_ship = %Ship{x: 100, size: 5}
+    assert {:error, "Invalid position"} = Board.add_ship(@player_id, invalid_position_ship)
+
+    invalid_orientation_ship = %Ship{size: 5, orientation: :invalid}
+    assert {:error, "Invalid orientation"} = Board.add_ship(@player_id, invalid_orientation_ship)
+
+    valid_ship = %Ship{x: 0, y: 0, size: 5, orientation: :vertical}
+    Board.add_ship(@player_id, valid_ship)
+    assert {:error, "Ship already added"} = Board.add_ship(@player_id, valid_ship)
+  end
+
+  test "addding valid ships" do
+    ship = %Ship{x: 0, y: 0, size: 5, orientation: :vertical}
+    assert {:ok, ^ship} = Board.add_ship(@player_id, ship)
+
+    ship = %Ship{x: 0, y: 0, size: 3, orientation: :vertical}
+    assert {:ok, ^ship} = Board.add_ship(@player_id, ship)
+
+    ship = %Ship{x: 0, y: 0, size: 3, orientation: :vertical}
+    assert {:ok, ^ship} = Board.add_ship(@player_id, ship)
+  end
+end
