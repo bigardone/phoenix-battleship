6dfa6c90cd40d8ee6a42aa25158eebdba4958cdf
Ricardo García Vega <bigardone@gmail.com>
2016-04-04 15:27:48 +0200

    Added stylus and base styles

--- brunch-config.js
+++ brunch-config.js
@@ -2,7 +2,7 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: 'js/app.js',
 
       // To use a separate vendor.js bundle, specify two files path
       // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
@@ -21,50 +21,75 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: 'css/app.css',
     },
     templates: {
-      joinTo: "js/app.js"
-    }
+      joinTo: 'js/app.js',
+    },
   },
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
     // By default, we set this to "/web/static/assets". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(web\/static\/assets)/
+    assets: /^(web\/static\/assets)/,
   },
 
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
     watched: [
-      "web/static",
-      "test/static"
+      'web/static',
+      'test/static',
     ],
 
     // Where to compile files to
-    public: "priv/static"
+    public: 'priv/static',
   },
 
   // Configure your plugins
   plugins: {
     babel: {
+      presets: ['es2015', 'react', 'stage-2', 'stage-0'],
+
       // Do not use ES6 compiler in vendor code
-      ignore: [/web\/static\/vendor/]
-    }
+      ignore: [/web\/static\/vendor/],
+    },
+
+    stylus: {
+      plugins: [
+      ],
+
+      linenos: true,
+    },
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["web/static/js/app"]
-    }
+      'js/app.js': ['web/static/js/app'],
+    },
   },
 
   npm: {
     enabled: true,
+
     // Whitelist the npm deps to be pulled in as front-end assets.
     // All other deps in package.json will be excluded from the bundle.
-    whitelist: ["phoenix", "phoenix_html"]
-  }
+    whitelist: [
+      'classnames',
+      'history',
+      'invariant',
+      'phoenix',
+      'phoenix_html',
+      'react',
+      'react-addons-css-transition-group',
+      'react-dom',
+      'react-redux',
+      'react-router',
+      'react-router-redux',
+      'redux',
+      'redux-logger',
+      'redux-thunk',
+    ],
+  },
 };
