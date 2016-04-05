c4c55305f0d22c67f22ef6a5cdc39b58b74353aa
Ricardo García Vega <bigardone@gmail.com>
2016-04-05 17:19:30 +0200

    Set player id on pages  controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule Battleship.PageController do
   use Battleship.Web, :controller
 
+  @id_length Application.get_env(:battleship, :id_length)
+
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", id: player_id()
   end
+
+  defp player_id, do: @id_length |> :crypto.strong_rand_bytes |> Base.encode64()
 end
