5b2101f9b6b19d874943fb4a9e999791734e48eb
Ricardo García Vega <bigardone@gmail.com>
2016-04-05 17:18:32 +0200

    Added environment variables

--- config/config.exs
+++ config/config.exs
@@ -19,6 +19,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :battleship,
+  ga_tracking_code: "",
+  id_length: 64
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
