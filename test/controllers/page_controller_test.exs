834c0a2b016e347e472808e79675a89c9b6f31e0
Ricardo García Vega <bigardone@gmail.com>
2016-04-06 16:58:21 +0200

    Fixed test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Battleship.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Battleship"
   end
 end
