d923d622e83b11de5de066162e605ba240b4dac6
Ricardo García Vega <bigardone@gmail.com>
2016-04-08 19:35:18 +0200

    Added missing orientation param

--- web/channels/game_channel.ex
+++ web/channels/game_channel.ex
@@ -48,7 +48,7 @@ defmodule Battleship.GameChannel do
     player = socket.assigns.player
     game_id = socket.assigns.game_id
 
-    ship = %Ship{x: ship["x"], y: ship["y"], size: ship["size"]}
+    ship = %Ship{x: ship["x"], y: ship["y"], size: ship["size"], orientation: String.to_atom(ship["orientation"])}
     Logger.debug "#{inspect ship}"
 
     case Board.add_ship(player.id, ship) do
