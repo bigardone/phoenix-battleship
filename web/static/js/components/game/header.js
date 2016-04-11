e12a57b8d5c005bbb4e7a4f4f98638aa70df3562
Ricardo García Vega <bigardone@gmail.com>
2016-04-11 12:26:46 +0200

    Set title depending on boards statuses

--- web/static/js/components/game/header.js
+++ web/static/js/components/game/header.js
@@ -4,9 +4,22 @@ export default class Header extends React.Component {
   _renderContent() {
     const { game } = this.props;
 
+    const title = this._titleText(game);
     return (
-      <h1>Place your ships</h1>
+      <h1>{::this._titleText(game)}</h1>
     );
+  }
+
+  _titleText(game) {
+    const { my_board, opponents_board } = game;
+
+    if (!my_board.ready) {
+      return 'Place your ships';
+    } else if (!opponents_board || !opponents_board.ready) {
+      return 'Waiting for opponent ';
+    } else {
+      return 'Let the battle begin';
+    }
   }
 
   render() {
