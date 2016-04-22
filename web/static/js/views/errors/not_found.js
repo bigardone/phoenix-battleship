042f52190e29310077cbf2d89b117ba7c9f65663
Ricardo García Vega <bigardone@gmail.com>
2016-04-22 07:27:25 +0200

    Better not found view

--- web/static/js/views/errors/not_found.js
+++ web/static/js/views/errors/not_found.js
@@ -1,14 +1,22 @@
 import React, {PropTypes} from 'react';
+import { connect }          from 'react-redux';
+import NewGameButton        from '../../components/game/new_game_button';
 
-export default class NotFoundView extends React.Component {
+class NotFoundView extends React.Component {
   render() {
+    const { playerChannel, dispatch } = this.props;
+
     return (
-      <div>
-        <h1>Arr!</h1>
+      <div id="not_found" className="view-container">
+        <h1>Yo Ho Ho, game not found!</h1>
+        <NewGameButton playerChannel={playerChannel} dispatch={dispatch}>Start new battle, arr!</NewGameButton>
       </div>
     );
   }
 }
 
-NotFoundView.propTypes = {
-};
+const mapStateToProps = (state) => (
+  state.session
+);
+
+export default connect(mapStateToProps)(NotFoundView);
